40e6df182689394f71cae4ccafd111080278b831
Alexis Guzman <alexis12989x@gmail.com>
2021-02-25 11:13:50 -0600

    Set up Home component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,33 @@
-const index = () => {
+import { useContext } from 'react';
+
+import { getItems } from '../lib/api';
+
+import { SearchContext } from '../context/SearchContext';
+
+import Item from '../components/Item';
+
+export const getServerSideProps = async () => {
+    const items = await getItems();
+
+    return { props: { items } };
+};
+
+const index = ({ items }) => {
+    const { item } = useContext(SearchContext);
+
     return (
-        <div>
-            <p>Home Component</p>
-        </div>
+        <main className='home'>
+            <section className='home__item'>
+                {item && <Item specific={true} {...item} />}
+            </section>
+
+            <section className='home__items'>
+                <h2 className='home__products'>Productos :</h2>
+                {items.map((item) => (
+                    <Item key={item.code} {...item} />
+                ))}
+            </section>
+        </main>
     );
 };
 
