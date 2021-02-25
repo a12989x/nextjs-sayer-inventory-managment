2cb830f5176c35f3f47e950c195f9d3b6df9d408
Alexis Guzman <alexis12989x@gmail.com>
2021-02-25 11:12:16 -0600

    Set up Header component

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,37 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { useContext } from 'react';
+
+import { SearchContext } from '../context/SearchContext';
+
 const Header = () => {
+    const { values, handleChange, getProduct } = useContext(SearchContext);
+
     return (
-        <div>
-            <p>Header Component</p>
-        </div>
+        <header className='header'>
+            <Link href='/'>
+                <a className='header__logo'>
+                    <h1 className='header__title'>Sayer</h1>
+                </a>
+            </Link>
+            <form className='header__header' onSubmit={getProduct}>
+                <input
+                    type='text'
+                    className='header__search'
+                    name='item'
+                    value={values.item}
+                    onChange={handleChange}
+                />
+            </form>
+            <button className='header__logOut'>
+                <Image
+                    className='header__svg'
+                    src='/assets/svg/log-out.min.svg'
+                    height='18'
+                    width='18'
+                />
+            </button>
+        </header>
     );
 };
 
