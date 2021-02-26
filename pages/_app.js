25eb3c503323d8c129d787e15c1174592149e393
Alexis Guzman <alexis12989x@gmail.com>
2021-02-26 13:23:10 -0600

    Import AuthContext and add getInicialProps function

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,47 @@
+import Router from 'next/router';
+import { parseCookies } from 'nookies';
+
 import Layout from '../components/Layout';
 
 import SearchContextProvider from '../context/SearchContext';
 
 import '../styles/main.scss';
+import AuthContextProvider from '../context/AuthContext';
 
 const MyApp = ({ Component, pageProps }) => {
     return (
         <SearchContextProvider>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
+            <AuthContextProvider>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </AuthContextProvider>
         </SearchContextProvider>
     );
+};
+
+const redirectUser = (ctx, location) => {
+    if (ctx.req) {
+        ctx.res.writeHead(302, { Location: location });
+        ctx.res.end();
+    } else {
+        Router.push(location);
+    }
+};
+
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+    let pageProps = {};
+    const jwt = parseCookies(ctx).jwt;
+
+    if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx);
+    }
+
+    if (!jwt && ctx.pathname !== '/login') {
+        redirectUser(ctx, '/login');
+    }
+
+    return { pageProps };
 };
 
 export default MyApp;
