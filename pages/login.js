c212c169d3f8e7540936756fd34a4543b91657a1
Alexis Guzman <alexis12989x@gmail.com>
2021-02-26 13:49:46 -0600

    Add labels for inputs

--- pages/login.js
+++ pages/login.js
@@ -9,17 +9,26 @@ const Login = () => {
         <main className='login'>
             <form className='login__form' onSubmit={handleSubmit}>
                 <h1 className='login__title'>Inicia Sesión</h1>
+                <label className='login__label' htmlFor='email'>
+                    E-mail :
+                </label>
                 <input
                     className='login__email'
                     type='email'
+                    id='email'
                     name='email'
                     value={values.email}
                     onChange={handleChange}
                     required
                 />
+
+                <label className='login__label' htmlFor='password'>
+                    Password :
+                </label>
                 <input
                     className='login__password'
                     type='password'
+                    id='password'
                     name='password'
                     value={values.password}
                     onChange={handleChange}
