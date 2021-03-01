5eeb39cc59b104c367f10626b522f37d4407349b
Alexis Guzman <alexis12989x@gmail.com>
2021-03-01 10:02:00 -0600

    Create SelectInput component

---
+++ components/SelectInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectInput = () => {
+    return (
+        <div>
+            <p>SelectInput Component</p>
+        </div>
+    );
+};
+
+export default SelectInput;
