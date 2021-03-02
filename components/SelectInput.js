c638c5c574bae3391d8fdeea787a061fe3b65b59
Alexis Guzman <alexis12989x@gmail.com>
2021-03-02 13:12:23 -0600

    Set up SelectInput component

--- components/SelectInput.js
+++ components/SelectInput.js
@@ -1,9 +1,24 @@
-import React from 'react';
-
-const SelectInput = () => {
+const SelectInput = ({ values, id, state, setState }) => {
     return (
-        <div>
-            <p>SelectInput Component</p>
+        <div className='selectInput'>
+            <select
+                className='selectInput__select'
+                id={id}
+                value={state.name}
+                onChange={(e) =>
+                    setState(
+                        values.filter(
+                            (value) => value.name === e.target.value
+                        )[0]
+                    )
+                }
+            >
+                {values.map((value) => (
+                    <option key={values.id} className='selectInput__option'>
+                        {value.name}
+                    </option>
+                ))}
+            </select>
         </div>
     );
 };
