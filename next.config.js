66ee6a412c866cb980eddaaf3aebe6c44fee7a21
Alexis Guzman <alexis12989x@gmail.com>
2021-03-11 13:58:15 -0600

    Add env variables for next-auth

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     env: {
         BASE_URL: process.env.BASE_URL,
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        NEXT_PUBLIC_DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     },
 };
